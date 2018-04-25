d9d33d974a07954a6453110101447ca24617f4ec
Anna Perkowska <perkowska.a@gmail.com>
2018-04-25 23:18:46 +0200

    Add new css styles

--- client/UserForm.js
+++ client/UserForm.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import styles from './UserForm.css';
+import cloud1 from './cloud1.png';
 
 class UserForm extends Component {
   constructor(props) {
@@ -18,16 +19,23 @@ class UserForm extends Component {
 
   render() {
     return(
-      <form className={styles.UserForm} onSubmit={e => this.handleSubmit(e)}>
-        <input
-          className={styles.UserInput}
-          placeholder='Write your nickname and press enter'
-          onChange={e => this.handleChange(e)}
-          value={this.state.name}
-        />
-      </form>
+      <div>
+        <div className={styles.WelcomeBox}>
+          <h1 className={styles.UserWelcome}>Welcome to our CHAT!</h1>
+          <img className={styles.Image} src={cloud1} alt={"cloud"} />
+        </div>
+        <form className={styles.UserForm} onSubmit={e => this.handleSubmit(e)}>
+          <input
+            className={styles.UserInput}
+            placeholder='Write your nickname and press enter'
+            onChange={e => this.handleChange(e)}
+            value={this.state.name}
+          />
+        </form>
+      </div>
     );
   }
 }
 
 export default UserForm;
+
--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,15 @@ module.exports = {
 						}
 					}
 				]
+			},
+			{
+				test: /\.(png|jpg|gif)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {}
+					}
+				]
 			}
 		]
 	},
